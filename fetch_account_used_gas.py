86b47800e197b56f5dadf043135e506987b8edff
Dmitry Dmitriev <dmitriev.dd@hotmail.com>
2021-09-15 00:45:28 +0300

    refactoring of fetch_account_used_gas.py

--- fetch_account_used_gas.py
+++ fetch_account_used_gas.py
@@ -6,8 +6,6 @@
 import logging
 import os
 import sys
-import time
-import requests
 
 import etherscan
 
@@ -15,17 +13,7 @@ from decimal import *
 
 getcontext().prec = 18
 
-logging.basicConfig(format='%(levelname)s\t\t| %(message)s', level=logging.INFO)
-
-# API key for https://nownodes.io/
-secret_api_key = os.environ.get('ETHERSCAN_API_KEY')
-if secret_api_key == None or secret_api_key == "":
-    logging.critical("API key must be set in env variable ETHERSCAN_API_KEY")
-    exit(1)
-
-ETH_CURRENCY_PARTS = 1_000_000_000_000_000_000
-
-url = "https://api.etherscan.io/api"
+logging.basicConfig(format='%(levelname)s\t| %(message)s', level=logging.INFO)
 
 
 def get_accounts_from_file():
@@ -41,39 +29,17 @@ def get_accounts_from_file():
 
 
 def get_account_fees(account):
-    data = etherscan.account_fees(secret_api_key, url, account)
-
-    logging.info("server resp is '" + data["message"] + "'")
-
-    if "result" not in data:
-        logging.critical("incorrect server response: " + str(data))
-        exit(1)
-
-    if data["message"] == "NOTOK":
-        logging.critical("error processing request, resp: " + str(data))
-        exit(1)
-
-    # params = dict(
-    #     module="account",
-    #     action="tokentx",
-    #     address=account,
-    #     sort="asc",
-    #     apikey=secret_api_key
-    # )
-    # resp = requests.get(url=url, params=params)
-    # tokentx = resp.json()
-
-    # logging.info(tokentx)
+    data = etherscan.account_normal_transactions(account)
 
     # Calculate transaction fee
     fees = {}
-    for item in data["result"]:
+    for item in data:
         logging.debug(item)
         gas_price = int(item["gasPrice"])
         gas_used = int(item["gasUsed"])
 
         if item["contractAddress"] != "":
-            logging.warning("found contrack address " + item["contractAddress"])
+            logging.warning("found contract address " + item["contractAddress"])
 
         if item["isError"] != "0":
             logging.debug("isError is set for " + item["hash"])
@@ -83,16 +49,16 @@ def get_account_fees(account):
             continue
 
         current_fee = gas_price * gas_used
-        logging.debug(str(gas_price/ETH_CURRENCY_PARTS) + ", " +
-                      str(gas_used/ETH_CURRENCY_PARTS) + ", " +
-                      str(current_fee/ETH_CURRENCY_PARTS))
+        logging.debug(str(gas_price/etherscan.ETH_CURRENCY_PARTS) + ", " +
+                      str(gas_used/etherscan.ETH_CURRENCY_PARTS) + ", " +
+                      str(current_fee/etherscan.ETH_CURRENCY_PARTS))
 
         contract_address = "empty"
         if item["contractAddress"] != "":
             contract_address = item["contractAddress"]
 
         fees[item["hash"]] = {
-            "fee": current_fee / ETH_CURRENCY_PARTS,
+            "fee": current_fee / etherscan.ETH_CURRENCY_PARTS,
             "fee_gwei": current_fee,
             "contract": contract_address,
         }
@@ -109,7 +75,6 @@ def main(argv):
     results = {}
     acc_idx = 1
     for account in accounts:
-        logging.info("----------------------------------------------")
         logging.info("[" + str(acc_idx) + "/" + str(len(accounts)) +
                      "] calculate fee for account " + account)
 
@@ -122,14 +87,14 @@ def main(argv):
         total_fee = 0
         for result in results:
             for transaction in results[result]:
-                print(results[result][transaction]["fee_gwei"])
                 total_fee += results[result][transaction]["fee_gwei"]
                 csv_file.write(result + ", " +
                                str(transaction) + ", " +
                                str(results[result][transaction]["contract"]) + ", " +
                                str(results[result][transaction]["fee_gwei"]) +
                                os.linesep)
-        print(Decimal(total_fee) / Decimal(ETH_CURRENCY_PARTS))
+                               
+        print(Decimal(total_fee) / Decimal(etherscan.ETH_CURRENCY_PARTS))
 
 
 if __name__ == '__main__':
